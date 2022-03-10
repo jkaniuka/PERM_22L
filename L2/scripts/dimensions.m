95df1e0ebd54cfbe80b6d81862c886b86c5cee79
jkaniuka <j.kaniuka@stud.elka.pw.edu.pl>
2022-03-10 02:46:32 +0100

    something works

---
+++ L2/scripts/dimensions.m
@@ -0,0 +1,3 @@
+https://www.mathworks.com/help/signal/ref/findpeaks.html
+https://www.mathworks.com/help/matlab/ref/islocalmax.html#d123e783724
+https://www.mathworks.com/help/matlab/ref/islocalmin.html
